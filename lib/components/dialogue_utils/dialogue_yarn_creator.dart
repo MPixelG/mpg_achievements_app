dd490f011667a1a6f4cd6f78d650cf890c7c4fac
LetzTiz <115793599+LetzTiz@users.noreply.github.com>
2025-09-27 15:07:55 +0200

    Progress 1 on a yarn progress save will do more later

--- lib/components/dialogue_utils/dialogue_yarn_creator.dart
+++ lib/components/dialogue_utils/dialogue_yarn_creator.dart
@@ -1,28 +1,31 @@
-
 import 'dart:async';
-
 import 'package:flame/components.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:jenny/jenny.dart';
+import 'package:mpg_achievements_app/components/state_management/providers/taskStateProvider.dart';
 
 class DialogueYarnCreator extends Component with DialogueView {
+  final WidgetRef ref; // <<--- wichtig!
+  DialogueYarnCreator(this.ref);
 
   late YarnProject project;
   late String yarnFilePath;
   late String script;
-
 
   @override
   Future<void> onLoad() async {
     await super.onLoad();
     await loadYarnFile('assets/yarn/test.yarn');
   }
-// Parse the Yarn script into a YarnProject
+
   Future<YarnProject> loadYarnFile(String yarnfile) async {
     script = await rootBundle.loadString(yarnfile);
     project = YarnProject()
       ..commands.addCommand0('playeryes', playeryes as FutureOr<void> Function())
       ..commands.addCommand0('playerno', playerno as FutureOr<void> Function() )
+      ..commands.addCommand1<String>('progressStart', progressStart)
+      ..commands.addCommand2<String, int>('progressUpdate', progressUpdate)
       ..parse(script);
     return project;
   }
@@ -33,8 +36,17 @@ class DialogueYarnCreator extends Component with DialogueView {
 
   void playerno() {
     print('no');
-      }
   }
+
+  void progressStart(String taskId) {
+    ref.read(taskProvider.notifier).startTask(taskId);
+  }
+
+  void progressUpdate(String taskId, int amount) {
+    ref.read(taskProvider.notifier).updateProgress(taskId, amount);
+  }
+}
+
 
 
 
