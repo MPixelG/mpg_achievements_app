8a9a92bc85b72003e86d5768c689a6671a89cf66
Florebore <florian.boettinger@gmail.com>
2025-09-27 14:10:44 +0200

    changed structure and constructor of statemanagement for task

---
+++ lib/components/state_management/providers/taskStateProvider.dart
@@ -0,0 +1,72 @@
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mpg_achievements_app/components/state_management/models/TaskData.dart';
+import 'package:mpg_achievements_app/components/state_management/tasks/TaskStateManagement.dart';
+
+
+// Provider global definieren
+final taskProvider =  NotifierProvider<TaskNotifier, List<TaskData>> (TaskNotifier.new);
+
+class TaskNotifier extends Notifier<List<TaskData>> {
+
+  @override
+  List<TaskData> build() {
+    // TODO: implement build
+    //used to initialise tasklist for level or game in general
+    throw UnimplementedError();
+  }
+
+
+  /// Task hinzufügen
+  void addTask(String description, {int goal = 1}) {
+    state = [
+      ...state,
+      TaskData(id: uuid.v4(), description: description, goal: goal),
+    ];
+  }
+
+  /// Task starten
+  void startTask(String id) {
+    state = [
+      for (final task in state)
+        if (task.id == id)
+          (task..start()) // ruft start() auf, gibt Task zurück
+        else
+          task
+    ];
+  }
+
+  /// Fortschritt updaten
+  void updateProgress(String id, int amount) {
+    state = [
+      for (final task in state)
+        if (task.id == id)
+          (task..updateProgress(amount))
+        else
+          task
+    ];
+  }
+
+  /// Task direkt abschließen
+  void completeTask(String id) {
+    state = [
+      for (final task in state)
+        if (task.id == id)
+          (task..complete())
+        else
+          task
+    ];
+  }
+
+  /// Task fehlschlagen lassen
+  void failTask(String id) {
+    state = [
+      for (final task in state)
+        if (task.id == id)
+          (task..fail())
+        else
+          task
+    ];
+  }
+}
+
