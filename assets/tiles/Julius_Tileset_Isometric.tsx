2f191e499a20dc7d18ed7b2e0cf0385d40ae20c0
Gamer4003 <julian.schmid5@web.de>
2025-09-29 17:09:18 +0200

    Adding a new Level (Level_9)

--- assets/tiles/Julius_Tileset_Isometric.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="Julius_Tileset_Isometric" tilewidth="32" tileheight="16" tilecount="286" columns="11">
- <image source="../images/Pixel_ArtTop_Down/iso_sprite_sheet.png" width="352" height="416"/>
-</tileset>
